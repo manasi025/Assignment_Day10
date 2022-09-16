01d276b5a0d18f69eb94fdb69696a1007acf531e
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-09-17 01:45:25 +0530

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -4,7 +4,7 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Welcome to Address Book Program!!");
 		Contact contact1 = new Contact("Ram ","Kumar ","Nashik Road ","Nashik ","Maharashtra","rk025@gmail.com ", 422101 , 8754321);
 	}
 
@@ -15,11 +15,13 @@ public class AddressBook {
 
 OUTPUT :
 
+Welcome to Address Book Program!!
+
 First_Name : Ram    Last_Name : Kumar 
 Address : Nashik Road 
 City : Nashik     State : Maharashtra
 Zipcode : 422101
 Phone No : 8754321
-Email : rk025@gmail.com  
+Email : rk025@gmail.com   
 
 */
