1462c18748651301b45111fd7785bdc3da6aea2e
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-09-17 01:39:41 +0530

    Add files via upload

---
+++ AddressBook.java
@@ -0,0 +1,11 @@
+package com.day10;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Contact contact1 = new Contact("Ram ","Kumar ","Nashik Road ","Nashik ","Maharashtra","rk025@gmail.com ", 422101 , 8754321);
+	}
+
+}
---
+++ Contact.java
@@ -0,0 +1,29 @@
+package com.day10;
+
+public class Contact 
+{
+	String firstName; 
+	String lastName;
+	String address;
+	String city;
+	String state;
+	String  email;
+	int zipCode;
+	long phoneNo;
+	
+	public Contact(String first_Name, String last_Name, String address, String city, String state, String email,
+			int zipCode, long phoneNo) {
+		
+		this.firstName = first_Name;
+		this.lastName = last_Name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.zipCode = zipCode;
+		this.phoneNo = phoneNo;
+		System.out.println("First_Name : " + firstName + "   Last_Name : " + lastName + "\nAddress : " + address + "\nCity : " + city +
+				"    State : " + state + "\nZipcode : " + zipCode + "\nPhone No : " + phoneNo + "\nEmail : " + email + "  ");
+	
+	}
+}
